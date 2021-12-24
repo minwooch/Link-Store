0ee9d7d9dcba02f485bf6949e7486788017eadda
minwooch <79406120+minwooch@users.noreply.github.com>
2021-12-24 19:40:17 +0900

    힐트 마이그레이션 - 서브 컴포넌트 SavedStateViewModelFactoryComponent를 힐트 entry point로 변경

--- app/src/main/java/com/applsh1205/linkstore/inject/SavedStateViewModelFactoryComponent.kt
+++ app/src/main/java/com/applsh1205/linkstore/inject/SavedStateViewModelFactoryComponent.kt
@@ -1,9 +1,16 @@
 package com.applsh1205.linkstore.inject
 
+import android.app.Activity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.savedstate.SavedStateRegistryOwner
 import dagger.BindsInstance
+import dagger.Module
+import dagger.Provides
 import dagger.Subcomponent
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 
 @Subcomponent
 interface SavedStateViewModelFactoryComponent {
@@ -19,4 +26,27 @@ interface SavedStateViewModelFactoryComponent {
     fun savedStateViewModelComponentFactory(): SavedStateViewModelComponent.Factory
 
     fun savedStateViewModelFactory(): SavedStateViewModelFactory
+}
+
+@InstallIn(ActivityComponent::class)
+@EntryPoint
+interface SavedStateViewModelEntryPoint {
+
+    fun savedStateViewModelComponentFactory(): SavedStateViewModelComponent.Factory
+
+    fun savedStateViewModelFactory(): SavedStateViewModelFactory
+}
+
+@InstallIn(ActivityComponent::class)
+@Module
+object ActivityModule {
+    @Provides
+    fun provideOwner(activity: Activity): SavedStateRegistryOwner {
+        return (activity as ComponentActivity)
+    }
+
+    @Provides
+    fun provideArgs(activity: Activity): Bundle? {
+        return activity.intent.extras
+    }
 }
