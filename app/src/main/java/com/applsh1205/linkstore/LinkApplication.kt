615c2f2a778e262b4058a9317a1e89076270391c
minwooch <79406120+minwooch@users.noreply.github.com>
2022-01-03 17:55:09 +0900

    힐트 마이그레이션 - AppComponent 제거

--- app/src/main/java/com/applsh1205/linkstore/LinkApplication.kt
+++ app/src/main/java/com/applsh1205/linkstore/LinkApplication.kt
@@ -1,12 +1,9 @@
 package com.applsh1205.linkstore
 
 import android.app.Application
-import com.applsh1205.linkstore.inject.AppComponent
-import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class LinkApplication : Application() {
 
-    val appComponent: AppComponent by lazy { EntryPointAccessors.fromApplication(this, AppComponent::class.java) }
 }
--- app/src/main/java/com/applsh1205/linkstore/inject/AppScope.kt
+++
@@ -1,7 +0,0 @@
-package com.applsh1205.linkstore.inject
-
-import javax.inject.Scope
-
-@Scope
-@Retention(AnnotationRetention.RUNTIME)
-annotation class AppScope
